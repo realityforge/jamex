2782de68a1ad33ab32eaba5f9896f060d65bce39
Peter Donald <peter@realityforge.org>
2010-05-24 21:00:19 +1000

    Define a base class for cotnainer implementations

---
+++ vendor/buildr/buildr-osgi-runtime/lib/buildr/osgi/container.rb
@@ -0,0 +1,40 @@
+module Buildr
+  module OSGi
+    class Container
+      attr_reader :runtime
+
+      def initialize(runtime)
+        @runtime = runtime
+        @parameters = {}
+      end
+
+      def []=(key, value)
+        @parameters[key] = value
+      end
+
+      def [](key)
+        @parameters[key]
+      end
+
+      def configuration_dir
+        File.dirname(configuration_file)
+      end
+
+      def bundle_dir
+        "lib"
+      end
+
+      def bundles
+        raise "bundles should be overidden"
+      end
+
+      def configuration_file
+        raise "configuration_file should be overidden"
+      end
+
+      def write_config(file)
+        raise "generate_config should be overidden"
+      end
+    end
+  end
+end
--- vendor/buildr/buildr-osgi-runtime/lib/buildr_osgi_runtime.rb
+++ vendor/buildr/buildr-osgi-runtime/lib/buildr_osgi_runtime.rb
@@ -2,6 +2,7 @@ require 'buildr'
 require File.expand_path(File.dirname(__FILE__) + '/check_osgi.rb')
 require File.expand_path(File.dirname(__FILE__) + '/buildr/osgi/bundle.rb')
 require File.expand_path(File.dirname(__FILE__) + '/buildr/osgi/feature.rb')
+require File.expand_path(File.dirname(__FILE__) + '/buildr/osgi/container.rb')
 require File.expand_path(File.dirname(__FILE__) + '/buildr/osgi/runtime.rb')
 require File.expand_path(File.dirname(__FILE__) + '/buildr/osgi/project_extension.rb')
 
