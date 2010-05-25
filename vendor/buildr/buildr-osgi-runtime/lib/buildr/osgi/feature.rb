5ff9ad11fbc3f62b21183fcf133e012db0c6fa09
Peter Donald <peter@realityforge.org>
2010-05-25 20:36:40 +1000

    Allow features to provide default values for specific system properties

--- vendor/buildr/buildr-osgi-runtime/lib/buildr/osgi/feature.rb
+++ vendor/buildr/buildr-osgi-runtime/lib/buildr/osgi/feature.rb
@@ -3,10 +3,12 @@ module Buildr
     class Feature
       attr_reader :feature_key
       attr_accessor :bundles
+      attr_reader :system_properties
 
       def initialize(feature_key)
         @feature_key = feature_key
         @bundles = []
+        @system_properties = OrderedHash.new
       end
     end
   end
