718c69968d1117ebbb6f99b7f125cac97994115f
Peter Donald <peter@realityforge.org>
2010-05-27 21:10:28 +1000

    Define features ... that may be removed later

--- vendor/buildr/buildr-osgi-runtime/lib/buildr/osgi/features.rb
+++ vendor/buildr/buildr-osgi-runtime/lib/buildr/osgi/features.rb
@@ -64,6 +64,60 @@ module Buildr
             Bundle.new("com.buschmais.maexo.modules.server:maexo-server.platform:jar:1.0.0", 60),
         ]
       end
+
+      def define_jee_bundles
+        [
+            Bundle.new("org.ow2.spec.ee:ow2-ejb-3.0-spec:jar:1.0.3", 45),
+            Bundle.new("org.ow2.spec.ee:ow2-ejb-3.1-spec:jar:1.0.3", 45),
+            Bundle.new("org.ow2.spec.ee:ow2-jpa-1.0-spec:jar:1.0.3", 45),
+            Bundle.new("org.ow2.spec.ee:ow2-jpa-2.0-spec:jar:1.0.3", 45),
+            Bundle.new("org.ow2.spec.ee:ow2-jta-1.1-spec:jar:1.0.3", 45),
+            Bundle.new("org.ow2.spec.ee:ow2-connector-1.5-spec:jar:1.0.3", 45),
+            Bundle.new("org.ow2.spec.ee:ow2-jsr77-1.1-spec:jar:1.0.3", 45),
+            Bundle.new("org.apache.geronimo.specs:geronimo-jacc_1.1_spec:jar:1.0-M2", 45),
+            Bundle.new("org.apache.tomcat:servlet-api:jar:6.0.13", 45),
+            Bundle.new("javax.activation:activation:jar:1.1", 45),
+            Bundle.new("javax.mail:mail:jar:1.4", 45),
+            #Bundle.new("javax.xml.bind:jar:jaxb-api:2.0", 45),
+            Bundle.new("org.apache.geronimo.specs:geronimo-jms_1.1_spec:jar:1.1", 45),
+            Bundle.new("org.apache.geronimo.specs:geronimo-jaxrpc_1.1_spec:jar:1.1", 45),
+#            Bundle.new('javax.ejb:com.springsource.javax.ejb:jar:3.0.0', 45),
+#            Bundle.new('javax.activation:com.springsource.javax.activation:jar:1.1.1', 45),
+#            Bundle.new('javax.xml.soap:com.springsource.javax.xml.soap:jar:1.3.0', 45),
+#            Bundle.new('javax.servlet:com.springsource.javax.servlet:jar:2.5.0', 45),
+#            Bundle.new('javax.xml.rpc:com.springsource.javax.xml.rpc:jar:1.1.0', 45),
+#            Bundle.new('org.apache.geronimo.specs:com.springsource.javax.management.j2ee:jar:1.2.0', 45),
+
+        ]
+      end
+
+      def define_easybeans_bundles
+        [
+            Bundle.new('org.ow2.easybeans.osgi:easybeans-core:jar:1.1.0', 55),
+            Bundle.new('org.ow2.easybeans.osgi:easybeans-agent:jar:1.1.0', 55),
+            Bundle.new('org.ow2.easybeans.osgi:easybeans-component-carol:jar:1.1.0', 55),
+            Bundle.new('org.ow2.easybeans.osgi:easybeans-component-event:jar:1.1.0', 55),
+            Bundle.new('org.ow2.easybeans.osgi:easybeans-component-hsqldb:jar:1.1.0', 55),
+            Bundle.new('org.ow2.easybeans.osgi:easybeans-component-jdbcpool:jar:1.1.0', 55),
+            Bundle.new('org.ow2.easybeans.osgi:easybeans-component-jmx:jar:1.1.0', 55),
+            Bundle.new('org.ow2.easybeans.osgi:easybeans-component-joram:jar:1.1.0', 55),
+            Bundle.new('org.ow2.easybeans.osgi:easybeans-component-jotm:jar:1.1.0', 55),
+            Bundle.new('org.ow2.easybeans.osgi:easybeans-component-quartz:jar:1.1.0', 55),
+            Bundle.new('org.ow2.easybeans.osgi:easybeans-component-statistic:jar:1.1.0', 55),
+
+            Bundle.new('org.apache.felix:org.apache.felix.dependencymanager:jar:2.0.1', 55),
+            Bundle.new('org.apache.felix:org.apache.felix.scr:jar:1.0.8', 55),
+
+
+            Bundle.new('org.ow2.bundles:ow2-bundles-externals-commons-modeler:jar:1.0.18', 55),
+            Bundle.new('org.ow2.bundles:ow2-util-i18n:jar:1.0.18', 55),
+            Bundle.new('org.ow2.bundles:ow2-util-log:jar:1.0.18', 55),
+            Bundle.new('org.ow2.bundles:ow2-util-event-api:jar:1.0.18', 55),
+            Bundle.new('org.ow2.bundles:ow2-util-event-impl:jar:1.0.18', 55),
+            Bundle.new('org.ow2.bundles:ow2-util-jmx-api:jar:1.0.18', 55),
+            Bundle.new('org.ow2.bundles:ow2-util-jmx-impl:jar:1.0.18', 55),
+        ]
+      end
     end
   end
 end
