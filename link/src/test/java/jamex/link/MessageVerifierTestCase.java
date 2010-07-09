56da2778a40f393736676ce7be9e2536bb813836
Peter Donald <peter@realityforge.org>
2010-07-09 20:53:47 +1000

    Add test for XSD verifier

--- link/src/test/java/jamex/link/MessageVerifierTestCase.java
+++ link/src/test/java/jamex/link/MessageVerifierTestCase.java
@@ -1,14 +1,18 @@
 package jamex.link;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
 import java.util.regex.Pattern;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class MessageVerifierTestCase
 {
   @Test
   public void regexVerifier()
-      throws Exception
+    throws Exception
   {
     final TestTextMessage message = new TestTextMessage( "1", "myMessage" );
     try
@@ -17,7 +21,8 @@ public class MessageVerifierTestCase
     }
     catch( final Exception e )
     {
-      fail( "Expected to be able to verify message" );
+      e.printStackTrace();
+      fail( "Expected to be able to verify message but got " + e );
     }
 
     try
@@ -29,5 +34,54 @@ public class MessageVerifierTestCase
     {
       assertEquals( "e.getMessage()", "Message with ID = 1 failed to match pattern \"Not.*Message\".", e.getMessage() );
     }
+  }
+
+  @Test
+  public void xsdVerifier()
+    throws Exception
+  {
+    final String xsd = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\" ?>\n" +
+                       "<xs:schema xmlns:xs=\"http://www.w3.org/2001/XMLSchema\">\n" +
+                       "  <xs:element name=\"a\">\n" +
+                       "    <xs:complexType>\n" +
+                       "      <xs:attribute name=\"orderid\" type=\"xs:string\" use=\"required\"/>\n" +
+                       "    </xs:complexType>" +
+                       "  </xs:element>\n" +
+                       "</xs:schema>\n";
+
+    final URL url = createURLForContent( xsd, "xsd" );
+    try
+    {
+      final TestTextMessage message = new TestTextMessage( "1", "<a orderid=\"x\"/>" );
+      MessageVerifier.newXSDVerifier( url ).verifyMessage( message );
+    }
+    catch( final Exception e )
+    {
+      e.printStackTrace();
+      fail( "Expected to be able to verify message but got " + e );
+    }
+
+    try
+    {
+      final TestTextMessage message = new TestTextMessage( "1", "<a xorderid=\"x\"/>" );
+      MessageVerifier.newXSDVerifier( url ).verifyMessage( message );
+      fail( "Expected to not be able to verify message" );
+    }
+    catch( final Exception e )
+    {
+      assertEquals( "e.getMessage()",
+                    "Message with ID = 1 failed to match XSD loaded from " + url + ".",
+                    e.getMessage() );
+    }
+  }
+
+  private URL createURLForContent( final String content, final String suffix )
+    throws IOException
+  {
+    final File file = File.createTempFile( MessageVerifier.class.getName() + "Test-", "." + suffix );
+    final FileOutputStream output = new FileOutputStream( file );
+    output.write( content.getBytes( "UTF-8" ) );
+    output.close();
+    return file.toURI().toURL();
   }
 }
