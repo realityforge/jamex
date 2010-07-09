09746f8bf57a27167bb83f4858b4ebb85d26743b
Peter Donald <peter@realityforge.org>
2010-07-09 21:44:14 +1000

    Cleanup, stylize and document class

--- link/src/main/java/jamex/link/MessageTransformer.java
+++ link/src/main/java/jamex/link/MessageTransformer.java
@@ -15,38 +15,52 @@ import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
+/**
+ * Abstract class for services that transform one messages.
+ * Instances of this class should be idempotent and thread-safe.
+ */
 public abstract class MessageTransformer
 {
-  public abstract Message transformMessage( final Session session,
-                                            final Message message )
-      throws Exception;
+  /**
+   * Return the message passed in parameter with the transformation applied.
+   *
+   * @param session the session associated with the message.
+   * @param message the message to transform.
+   * @return the transformed message.
+   * @throws Exception if there is a problem transforming message.
+   */
+  public abstract Message transformMessage( final Session session, final Message message )
+    throws Exception;
 
+  /**
+   * Create a transformer that expects an XML formatted TextMessage and attempts to
+   * apply an XSLT transform. Uses the underlying javax.xml.transform API. 
+   *
+   * @param url the url of XSLT sheet
+   */
   public static MessageTransformer newXSLTransformer( final URL url )
-      throws Exception
+    throws Exception
   {
     if( null == url ) throw new NullPointerException( "url" );
-
-    final Source xsltSource = new StreamSource( url.openStream() );
-    final TransformerFactory transFact = TransformerFactory.newInstance();
-    final Transformer transformer = transFact.newTransformer( xsltSource );
-
-    return new XslMessageVerifier( transformer );
+    final Source source = new StreamSource( url.openStream() );
+    final TransformerFactory factory = TransformerFactory.newInstance();
+    final Transformer transformer = factory.newTransformer( source );
+    return new XslMessageTransformer( transformer );
   }
 
-  private static class XslMessageVerifier
-      extends MessageTransformer
+  private static class XslMessageTransformer
+    extends MessageTransformer
   {
-    private final Transformer transformer;
+    private final Transformer m_transformer;
 
-    private XslMessageVerifier( final Transformer transformer )
+    private XslMessageTransformer( final Transformer transformer )
     {
-      this.transformer = transformer;
+      m_transformer = transformer;
     }
 
     @Override
-    public Message transformMessage( final Session session,
-                                     final Message message )
-        throws Exception
+    public Message transformMessage( final Session session, final Message message )
+      throws Exception
     {
       final TextMessage textMessage = MessageUtil.castToType( message, TextMessage.class );
       final String text = transformText( textMessage );
@@ -58,14 +72,13 @@ public abstract class MessageTransformer
     }
 
     private String transformText( final TextMessage textMessage )
-        throws JMSException, TransformerException
+      throws JMSException, TransformerException
     {
       final Source xmlSource = new StreamSource( new StringReader( textMessage.getText() ) );
       final ByteArrayOutputStream baos = new ByteArrayOutputStream();
       final Result result = new StreamResult( baos );
-      transformer.transform( xmlSource, result );
-      final String text = baos.toString();
-      return text;
+      m_transformer.transform( xmlSource, result );
+      return baos.toString();
     }
   }
 }
