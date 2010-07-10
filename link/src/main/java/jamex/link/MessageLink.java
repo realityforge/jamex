433383c2683092a75a69835c75257bb8d6ef0dee
Peter Donald <peter@realityforge.org>
2010-07-10 19:13:06 +1000

    Rename some of the variables

--- link/src/main/java/jamex/link/MessageLink.java
+++ link/src/main/java/jamex/link/MessageLink.java
@@ -11,124 +11,124 @@ import javax.jms.Topic;
 
 public final class MessageLink
 {
-  private String name;
-  private String sourceName;
-  private String subscriptionName;
-  private String selector;
-  private boolean isSourceTopic;
-  private MessageVerifier inputVerifier;
+  private String m_name;
+  private String m_sourceName;
+  private String m_subscriptionName;
+  private String m_selector;
+  private boolean m_isSourceTopic;
+  private MessageVerifier m_inputVerifier;
 
-  private String destinationName;
-  private boolean isDestinationTopic;
-  private MessageVerifier outputVerifier;
+  private String m_destinationName;
+  private boolean m_isDestinationTopic;
+  private MessageVerifier m_outputVerifier;
 
-  private String dmqName;
-  private MessageTransformer transformer;
+  private String m_dmqName;
+  private MessageTransformer m_transformer;
 
-  private boolean isFrozen;
+  private boolean m_isFrozen;
 
-  private Session session;
-  private MessageConsumer inConsumer;
-  private MessageProducer outProducer;
-  private MessageProducer dmqProducer;
+  private Session m_session;
+  private MessageConsumer m_inConsumer;
+  private MessageProducer m_outProducer;
+  private MessageProducer m_dmqProducer;
 
   public void setName( final String name )
   {
     ensureEditable();
-    this.name = name;
+    m_name = name;
   }
 
   public void setInputQueue( final String name, final String selector )
   {
     ensureEditable();
-    this.sourceName = name;
-    this.isSourceTopic = false;
-    this.subscriptionName = null;
-    this.selector = selector;
+    m_sourceName = name;
+    m_isSourceTopic = false;
+    m_subscriptionName = null;
+    m_selector = selector;
   }
 
   public void setInputTopic( final String name, final String subscription, final String selector )
   {
     ensureEditable();
     if( null == name ) throw new NullPointerException( "name" );
-    this.sourceName = name;
-    this.isSourceTopic = true;
-    this.subscriptionName = subscription;
-    this.selector = selector;
+    m_sourceName = name;
+    m_isSourceTopic = true;
+    m_subscriptionName = subscription;
+    m_selector = selector;
   }
 
   public void setOutputQueue( final String name )
   {
     ensureEditable();
     if( null == name ) throw new NullPointerException( "name" );
-    this.destinationName = name;
-    this.isDestinationTopic = false;
+    m_destinationName = name;
+    m_isDestinationTopic = false;
   }
 
   public void setOutputTopic( final String name )
   {
     ensureEditable();
-    this.destinationName = name;
-    this.isDestinationTopic = true;
+    m_destinationName = name;
+    m_isDestinationTopic = true;
   }
 
   public void setDmqName( final String dmqName )
   {
     ensureEditable();
-    this.dmqName = dmqName;
+    m_dmqName = dmqName;
   }
 
   public void setInputVerifier( final MessageVerifier inputVerifier )
   {
     ensureEditable();
-    this.inputVerifier = inputVerifier;
+    m_inputVerifier = inputVerifier;
   }
 
   public void setOutputVerifier( final MessageVerifier outputVerifier )
   {
     ensureEditable();
-    this.outputVerifier = outputVerifier;
+    m_outputVerifier = outputVerifier;
   }
 
   public void setTransformer( final MessageTransformer transformer )
   {
     ensureEditable();
-    this.transformer = transformer;
+    m_transformer = transformer;
   }
 
   public void start( final Session session )
       throws Exception
   {
     if( null == session ) throw invalid( "session must not be null" );
-    isFrozen = true;
+    m_isFrozen = true;
     try
     {
       ensureValidConfig();
 
-      this.session = session;
+      m_session = session;
 
       final Destination inChannel =
-          isSourceTopic ? session.createTopic( sourceName ) : session.createQueue( sourceName );
+          m_isSourceTopic ? m_session.createTopic( m_sourceName ) : m_session.createQueue( m_sourceName );
       final Destination outChannel =
-          isDestinationTopic ? session.createTopic( destinationName ) : session.createQueue( destinationName );
-      final Destination dmq = ( null != dmqName ) ? session.createQueue( dmqName ) : null;
+          m_isDestinationTopic ? m_session.createTopic( m_destinationName ) : m_session.createQueue( m_destinationName );
+      final Destination dmq = ( null != m_dmqName ) ? m_session.createQueue( m_dmqName ) : null;
 
-      outProducer = session.createProducer( outChannel );
-      dmqProducer = ( null != dmq ) ? session.createProducer( dmq ) : null;
+      m_outProducer = m_session.createProducer( outChannel );
+      m_dmqProducer = ( null != dmq ) ? m_session.createProducer( dmq ) : null;
 
-      if( null != subscriptionName )
+      if( null != m_subscriptionName )
       {
-        inConsumer = session.createDurableSubscriber( (Topic)inChannel, subscriptionName, selector, true );
+        m_inConsumer = m_session.createDurableSubscriber( (Topic)inChannel, m_subscriptionName, m_selector, true );
       }
       else
       {
-        inConsumer = session.createConsumer( inChannel, selector );
+        m_inConsumer = m_session.createConsumer( inChannel, m_selector );
       }
-      inConsumer.setMessageListener( new LinkMessageListener() );
+      m_inConsumer.setMessageListener( new LinkMessageListener() );
     }
     catch( final JMSException e )
     {
-      isFrozen = false;
+      m_isFrozen = false;
       log( "Error starting MessageLink", e );
       stop();
       throw e;
@@ -140,52 +140,52 @@ public final class MessageLink
   {
     try
     {
-      if( null != inConsumer ) inConsumer.close();
+      if( null != m_inConsumer ) m_inConsumer.close();
     }
     catch( final JMSException e )
     {
       log( "Closing consumer", e );
     }
-    inConsumer = null;
+    m_inConsumer = null;
 
     try
     {
-      if( null != outProducer ) outProducer.close();
+      if( null != m_outProducer ) m_outProducer.close();
     }
     catch( final JMSException e )
     {
       log( "Closing producer", e );
     }
-    outProducer = null;
+    m_outProducer = null;
 
     try
     {
-      if( null != dmqProducer ) dmqProducer.close();
+      if( null != m_dmqProducer ) m_dmqProducer.close();
     }
     catch( final JMSException e )
     {
       log( "Closing producer for dmq", e );
     }
-    dmqProducer = null;
+    m_dmqProducer = null;
 
     try
     {
-      if( null != session ) session.close();
+      if( null != m_session ) m_session.close();
     }
     catch( final JMSException e )
     {
       log( "Closing session", e );
     }
-    session = null;
+    m_session = null;
 
-    isFrozen = false;
+    m_isFrozen = false;
   }
 
   private void doMessage( final Message message )
   {
     try
     {
-      if( null != inputVerifier ) inputVerifier.verifyMessage( message );
+      if( null != m_inputVerifier ) m_inputVerifier.verifyMessage( message );
     }
     catch( final Exception e )
     {
@@ -196,7 +196,7 @@ public final class MessageLink
     final Message output;
     try
     {
-      if( null != transformer ) output = transformer.transformMessage( session, message );
+      if( null != m_transformer ) output = m_transformer.transformMessage( m_session, message );
       else output = message;
     }
     catch( final Exception e )
@@ -215,7 +215,7 @@ public final class MessageLink
                               final Throwable t )
   {
     log( reason, t );
-    if( null == dmqProducer )
+    if( null == m_dmqProducer )
     {
       final String message = "Unable to handle message and no DMQ to send message to. Message: " + inMessage;
       throw new IllegalStateException( message );
@@ -223,17 +223,17 @@ public final class MessageLink
     final Message message = inMessage;
     try
     {
-      message.setStringProperty( "JMLMessageLink", name );
+      message.setStringProperty( "JMLMessageLink", m_name );
       message.setStringProperty( "JMLFailureReason", reason );
-      message.setStringProperty( "JMLInChannelName", sourceName );
-      message.setStringProperty( "JMLInChannelType", isSourceTopic ? "Topic" : "Queue" );
-      if( null != subscriptionName )
+      message.setStringProperty( "JMLInChannelName", m_sourceName );
+      message.setStringProperty( "JMLInChannelType", m_isSourceTopic ? "Topic" : "Queue" );
+      if( null != m_subscriptionName )
       {
-        message.setStringProperty( "JMLInSubscriptionName", subscriptionName );
+        message.setStringProperty( "JMLInSubscriptionName", m_subscriptionName );
       }
-      message.setStringProperty( "JMLOutChannelName", destinationName );
-      message.setStringProperty( "JMLOutChannelType", isDestinationTopic ? "Topic" : "Queue" );
-      dmqProducer.send( message,
+      message.setStringProperty( "JMLOutChannelName", m_destinationName );
+      message.setStringProperty( "JMLOutChannelType", m_isDestinationTopic ? "Topic" : "Queue" );
+      m_dmqProducer.send( message,
                         message.getJMSDeliveryMode(),
                         message.getJMSPriority(),
                         message.getJMSExpiration() );
@@ -250,7 +250,7 @@ public final class MessageLink
   {
     try
     {
-      if( null != outputVerifier ) outputVerifier.verifyMessage( outMessage );
+      if( null != m_outputVerifier ) m_outputVerifier.verifyMessage( outMessage );
     }
     catch( final Exception e )
     {
@@ -258,7 +258,7 @@ public final class MessageLink
     }
     try
     {
-      outProducer.send( outMessage,
+      m_outProducer.send( outMessage,
                         outMessage.getJMSDeliveryMode(),
                         outMessage.getJMSPriority(),
                         outMessage.getJMSExpiration() );
@@ -272,9 +272,9 @@ public final class MessageLink
   private void ensureValidConfig()
       throws Exception
   {
-    if( null == sourceName ) throw invalid( "sourceName not specified" );
-    else if( null == destinationName ) throw invalid( "sourceName not specified" );
-    else if( null != subscriptionName && !isSourceTopic )
+    if( null == m_sourceName ) throw invalid( "sourceName not specified" );
+    else if( null == m_destinationName ) throw invalid( "sourceName not specified" );
+    else if( null != m_subscriptionName && !m_isSourceTopic )
     {
       throw invalid( "subscriptionName should only be specified for topics" );
     }
@@ -282,25 +282,25 @@ public final class MessageLink
 
   private Exception invalid( final String message )
   {
-    return new IllegalStateException( "MessageLink (" + name + ") invalid. Reason: " + message );
+    return new IllegalStateException( "MessageLink (" + m_name + ") invalid. Reason: " + message );
   }
 
   private void log( final String message, final Throwable t )
   {
     synchronized( System.out )
     {
-      System.out.println( "MessageLink (" + name + ") Error: " + message );
+      System.out.println( "MessageLink (" + m_name + ") Error: " + message );
       if( null != t ) t.printStackTrace( System.out );
     }
   }
 
   private void ensureEditable()
   {
-    if( isFrozen )
+    if( m_isFrozen )
     {
       final String message = "Attempting to edit active MessageLink";
       log( message, null );
-      throw new IllegalStateException( "MessageLink (" + name + "). " + message );
+      throw new IllegalStateException( "MessageLink (" + m_name + "). " + message );
     }
   }
 
