ca4513045b25ad7b4880407ca27d84ca814dddfd
Peter Donald <peter@realityforge.org>
2010-07-10 12:44:48 +1000

    Ensure there is at most one broker started during nested startup/shjutdown calls

--- link/src/test/java/jamex/link/TestHelper.java
+++ link/src/test/java/jamex/link/TestHelper.java
@@ -20,7 +20,8 @@ final class TestHelper
   static final String TOPIC_2_NAME = "TOPIC_2";
   static final String QUEUE_1_NAME = "QUEUE_1";
   static final String QUEUE_2_NAME = "QUEUE_2";
-  static BrokerService c_broker;
+  private static BrokerService c_broker;
+  private static int c_initDepth;
 
   static URL createURLForContent( final Class<?> type, final String content, final String suffix )
     throws IOException
@@ -35,43 +36,52 @@ final class TestHelper
   static void startupBroker()
     throws Exception
   {
-    if( null != c_broker )
+    verifyState();
+    if( 0 == c_initDepth )
     {
-      throw new IllegalStateException( "Expected broker to be uninitialized");
+      final ActiveMQDestination[] destinations =
+        {
+          new ActiveMQQueue( DMQ_NAME ),
+          new ActiveMQTopic( TOPIC_1_NAME ),
+          new ActiveMQTopic( TOPIC_2_NAME ),
+          new ActiveMQQueue( QUEUE_1_NAME ),
+          new ActiveMQQueue( QUEUE_2_NAME ),
+        };
+      final BrokerService broker = new BrokerService();
+      broker.setBrokerName( BROKER_NAME );
+      broker.setCacheTempDestinations( false );
+      broker.setDestinations( destinations );
+      broker.setPersistent( false );
+      broker.setUseJmx( false );
+      broker.setUseShutdownHook( false );
+      broker.start();
+      c_broker = broker;
     }
-    final ActiveMQDestination[] destinations =
-      {
-        new ActiveMQQueue( DMQ_NAME ),
-        new ActiveMQTopic( TOPIC_1_NAME ),
-        new ActiveMQTopic( TOPIC_2_NAME ),
-        new ActiveMQQueue( QUEUE_1_NAME ),
-        new ActiveMQQueue( QUEUE_2_NAME ),
-      };
-    final BrokerService broker = new BrokerService();
-    broker.setBrokerName( BROKER_NAME );
-    broker.setCacheTempDestinations( false );
-    broker.setDestinations( destinations );
-    broker.setPersistent( false );
-    broker.setUseJmx( false );
-    broker.setUseShutdownHook( false );
-    broker.start();
-    c_broker = broker;
+    c_initDepth++;
   }
 
   static void shutdownBroker()
     throws Exception
   {
-    if( null != c_broker )
+    verifyState();
+    if( 0 == c_initDepth )
     {
       final BrokerService broker = c_broker;
       c_broker = null;
       broker.stop();
     }
+    c_initDepth--;
   }
 
   static Connection createConnection()
     throws JMSException
   {
     return new ActiveMQConnectionFactory( "vm://" + BROKER_NAME + "?create=false" ).createConnection();
+  }
+
+  private static void verifyState()
+  {
+    if( 0 > c_initDepth ) throw new IllegalStateException( "0 > c_initDepth" );
+    if( 0 < c_initDepth && null == c_broker ) throw new IllegalStateException( "0 < c_initDepth && null == c_broker" );
   }
 }
